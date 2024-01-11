2a926569d55fa2a46718b2ff2b076ad218c06176
Zaid627 <154814781+Zaid627@users.noreply.github.com>
2024-01-11 16:17:47 +0500

    Update api.js

--- server/client/src/service/api.js
+++ server/client/src/service/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // const usersUrl = 'http://localhost:3003/users';
-const usersUrl = 'http://localhost:8080';
+const usersUrl = 'https://mern-crud6.vercel.app';
 
 export const getUsers = async (id) => {
     id = id || '';
